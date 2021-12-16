9bbcc9c9caf9839470c8404c10be35c026d52f36
Rahul <rsgupta848@gmail.com>
2021-12-16 21:42:59 +0530

    updated power of two

---
+++ Basic/power of two/Q1.cpp
@@ -0,0 +1,25 @@
+/*
+Power of two.
+Solution:
+    step1:Any number power of two has only one "1" in its binary representaion.
+    step2:Taking bitwise & of the number with one number less,will return 0.
+    step3:check the state of output and print the answer true or false.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    if((n&n-1)==0)
+    {
+        cout<<"true";
+    }
+    else
+    {
+        cout<<"false";
+    }
+    return 0;
+}
