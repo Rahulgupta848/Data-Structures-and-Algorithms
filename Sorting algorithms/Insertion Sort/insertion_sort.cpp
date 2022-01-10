7fd6ac61a9b9d79317a9c538a6e2c7a51350efe0
Rahul <rsgupta848@gmail.com>
2022-01-10 19:49:03 +0530

    Insertion Sort

--- Sorting algorithms/Insertion Sort/insertion_sort.cpp
+++ Sorting algorithms/Insertion Sort/insertion_sort.cpp
@@ -1,10 +1,28 @@
 /*
 Insertion sort.
-Time complexity : Best case=O(n)^2 ,Worst case=O(n).
+Time complexity : Worst case=O(n)^2 ,Best case=O(n).
 */
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int main() 
 {
+    int n=6;
+    int arr[n]={6,3,4,5,2,1};
+    for(int i=1;i<n;i++)
+    {
+        for(int j=i-1;j>=0;j--)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int i:arr)
+    {
+        cout<<i<<endl;
+    }
     return 0;
 }
