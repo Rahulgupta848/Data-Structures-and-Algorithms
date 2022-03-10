366bfe0f65ac1c3e91312b77ead71bf29f7c2610
Rahul <rsgupta848@gmail.com>
2022-03-10 17:28:56 +0530

    prefix evaluation

---
+++ stack/prefix evaluation/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def solve(v1,v2,operator):
+     if(operator=='+'):
+          return v2+v1
+     elif(operator=='-'):
+          return v2-v1
+     elif(operator=='/'):
+          return v2/v1
+     elif(operator=='*'):
+          return v2*v1
