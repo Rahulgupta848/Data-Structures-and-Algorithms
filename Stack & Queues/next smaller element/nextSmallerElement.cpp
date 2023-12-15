3b9454bcc7c3478f03c4db2d8299bde91c7716ab
unknown <rsgupta848@gmail.com>
2023-12-15 14:38:54 +0530

    vector and stack docs

---
+++ Stack & Queues/next smaller element/nextSmallerElement.cpp
@@ -0,0 +1,15 @@
+// Given an array, print the Next Smaller Element (NSE) for every element. 
+// The NSE for an element x is the first smaller element on the right side of x in the array. 
+// Elements for which no smaller element exist (on the right side), consider NSE as -1. 
+
+// Input: [4, 8, 5, 2, 25]
+// Output: [2, 5, 2, -1, -1]
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+     
+     return 0;
+}
