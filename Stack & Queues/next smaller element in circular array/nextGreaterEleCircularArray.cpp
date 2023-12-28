b31ecafcff51f3b5cfff01920d84b353c3ddc2de
unknown <rsgupta848@gmail.com>
2023-12-28 20:19:02 +0530

    resolved the ideal solution for next smaller element in circular array

--- Stack & Queues/next smaller element in circular array/nextGreaterEleCircularArray.cpp
+++ Stack & Queues/next smaller element in circular array/nextGreaterEleCircularArray.cpp
@@ -1,10 +1,10 @@
-// consider the array as circular array find next smaller array for given array
+// consider the array as circular array find next greater array for given array
 // e.g = [3,8,4,1,2]
 // ----->[8,-1,8,2,3]
 
 // solution:-
 //      1. traverse the array and set the initial state of stack such that element must be pushed only when 
-//           it is greater than its previous element because we are looking for next smaller elements from right side.
+//           it is greater than its previous element because we are looking for next greater elements from right side.
 
 #include<iostream>
 #include<vector>
@@ -21,10 +21,32 @@ void printArr(vector<int> arr){
 void findSolution(vector<int> arr){
      stack<int> s;
      s.push(-1);
-     // traverse the array and set the initial state of stack
+     vector<int> ans={0,0,0,0,0};
+     // traverse the array and set the initial state of stack such that pop the top of stack if it is smalller than current traversing element.
      for(int i = arr.size()-2;i>=0;i--){
-
+          if(s.top() == -1){
+               s.push(arr[i]);
+          }
+          else{
+               int el = arr[i];
+               while(s.top() != -1 && s.top() <= el){
+                    s.pop();
+               }
+               s.push(el);
+          }
      }
+     
+     // emplment next greater element 
+     for(int i = arr.size()-1;i>=0;i--){
+          int el = arr[i];
+          while(s.top() != -1 && s.top()<= el){
+               s.pop();
+          }
+          ans[i] = s.top();
+          s.push(el);
+     }
+
+     printArr(ans);
 }
 
 int main(){
